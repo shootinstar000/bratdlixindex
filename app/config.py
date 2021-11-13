f362086818da9f18a38a7a04934dbdbb2188c250
shootinstar000 <93067533+shootinstar000@users.noreply.github.com>
2021-11-13 14:18:38 +0530

    update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,14 @@ import os
 
 
 
-is = {"index_all":true,"index_private":false,"index_group":false,"index_channel":true,"exclude_chats":[],"include_chats":[]}
+i = {
+  "index_all": true,
+  "index_private": false,
+  "index_group": false,
+  "index_channel": true,
+  "exclude_chats": [],
+  "include_chats": []
+}
 
 
 try:
